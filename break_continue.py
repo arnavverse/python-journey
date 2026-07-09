394e7cb5b0f33cbed5838e4e075168be4a2bfcbd
Arnav Rathore <arnav005singhrathore@gmail.com>
2026-07-10 02:01:32 +0530

    Add new file

---
+++ break_continue.py
@@ -0,0 +1,21 @@
+z = int(input("ENTER THE NUMBER : "))
+for i in range(1,15):
+    print(z,"x",i,"=", z * i) 
+    if (i==10):
+        break
+
+x = 25
+for i in range(1,15):
+    if (i==10):
+        print("exit")
+        continue
+    print (x,"x",i,"=",x * i)
+
+while True:
+    number = int(input("Enter the positive number : "))
+    print(number)
+    if not number > 0 :
+        break
+
+for i in range(1,101):
+    print(i,"- KHTM 🤣💔")
