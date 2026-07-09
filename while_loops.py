f546d768c337b962a1e6e58e895e610447b9e42d
Arnav Rathore <arnav005singhrathore@gmail.com>
2026-07-10 00:03:01 +0530

    Add new file

---
+++ while_loops.py
@@ -0,0 +1,15 @@
+for i in range(0,9):
+    print(i)
+
+t = 0
+while (t<=6):
+    print(t)
+    t = t + 1
+print("Done with the loop")
+
+k = int(input("ENTER THE NUMBER : "))
+while(k>=0):
+    print(k)
+    k = k - 1
+else:
+     print("ok done!")
